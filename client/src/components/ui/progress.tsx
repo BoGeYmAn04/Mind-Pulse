299be5c29c92546a5eb740108f9d36549b0b1f6c
Priyash <priyashpatel13@gmail.com>
2026-09-03 01:50:10 +0530

    Fix UI radix for production

--- client/src/components/ui/progress.tsx
+++ client/src/components/ui/progress.tsx
@@ -1,7 +1,7 @@
-import * as React from "react"
-import { Progress as ProgressPrimitive } from "radix-ui"
+import * as React from "react";
+import * as ProgressPrimitive from "@radix-ui/react-progress";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 function Progress({
   className,
@@ -25,7 +25,7 @@ function Progress({
         }}
       />
     </ProgressPrimitive.Root>
-  )
+  );
 }
 
-export { Progress }
+export { Progress };
--- client/src/components/ui/separator.tsx
+++ client/src/components/ui/separator.tsx
@@ -1,7 +1,7 @@
-import * as React from "react"
-import { Separator as SeparatorPrimitive } from "radix-ui"
+import * as React from "react";
+import * as SeparatorPrimitive from "@radix-ui/react-separator";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 function Separator({
   className,
@@ -16,14 +16,12 @@ function Separator({
       orientation={orientation}
       className={cn(
         "shrink-0 bg-border",
-        orientation === "horizontal"
-          ? "h-px w-full"
-          : "h-full w-px",
+        orientation === "horizontal" ? "h-px w-full" : "h-full w-px",
         className
       )}
       {...props}
     />
-  )
+  );
 }
 
-export { Separator }
+export { Separator };
